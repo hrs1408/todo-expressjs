3267c885b63bd299b53043345a2c5ebfb413e259
tokyoHuyhieu <hieu.nguyenphanhuy@students.hueic.edu.vn>
2022-04-08 18:41:36 +0700

    upload expores

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const { engine } = require('express-handlebars');
+const path = require('path');
+const morgan = require('morgan');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('.hbs', engine({ extname: '.hbs' }));
+
+app.set('view engine', '.hbs');
+
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.listen(port, () => { console.log(`Server is running on port ${port}`) });
+
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+    res.render('home');
+});
+
+app.get("/product", (req, res) => {
+    res.render('product');
+});
